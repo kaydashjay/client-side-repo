854fa0a14df383830c8777c859238caac3146c50
kaydashjay <greenku@quincy.edu>
2017-06-29 14:38:07 -0400

    closure demo final

--- basic-js/closure demo/closure.js
+++ basic-js/closure demo/closure.js
@@ -11,19 +11,20 @@ var c = console;
     
 }
 
-function person(fname, lname){
-    return {
-        getFullName: function(){return fname+" "+lname;}
-    };
-
-}
-
 //execute outer and return an "inner" object
 var counter=outer();
 c.log(counter.getCount());
 c.log(counter.addToCount(10));
 c.log(counter.getCount());
 //c.log(counter.count);
+
+
+function person(fname, lname){
+    return {
+        getFullName: function(){return fname+" "+lname;}
+    };
+
+}
 
 //person function execution
 var joe = person ("joe", "k");
