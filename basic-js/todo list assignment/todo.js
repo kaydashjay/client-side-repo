babf211b4020449a92e13d6fbdac10ae0736ef73
kaydashjay <greenku@quincy.edu>
2017-06-29 19:50:12 -0400

    undo and deleting from both lists

--- basic-js/todo list assignment/todo.js
+++ basic-js/todo list assignment/todo.js
@@ -7,23 +7,43 @@ window.onload = function(){
     //get elements
     var textBox = d.getElementById("textbox");
     var addButton = d.getElementById("add");
-    var todolist = d.getElementById("todo-ul");
+    var todolist = d.getElementById("todo-ul");    
+    var completedList = d.getElementById("completed-ul");    
     var deletedlist = d.getElementById("deleted");
     var main = d.getElementById("main");
 
     //create new list item, its checkbox, and its delete button
         
 
-    //creates new item in todo list
-    function addItem (item){
+   
+
+    //event handling
+    addButton.addEventListener("click",function(){
+        if(textbox.value.trim()!="")
+        {
+            addItemTodo(textbox.value);
+        }
+        else
+        {
+            alert("Please enter a todo item");
+        }
+    }); 
+
+    
+    
+
+ //creates new item in todo list
+    function addItemTodo (item){
     var value=d.createTextNode(item);
      var li = d.createElement("li");
          var checkbox = d.createElement("input");
               var deleteButton = d.createElement("button");
 
-     checkbox.addEventListener("select", function() {
-        if (this.checked=="true"){
-            c.log("hey");
+     checkbox.addEventListener("change", function(){
+        if (this.checked){
+            event.currentTarget.parentElement.remove();
+            addItemCompleted(li.innerText);
+
         }
      
     });
@@ -41,27 +61,35 @@ window.onload = function(){
         todolist.appendChild(li);
         textbox.value="";
     }
+//////////////---------Add Item to Commpleted List---------------///////
+    function addItemCompleted (item){
+    var value=d.createTextNode(item);
+     var li = d.createElement("li");
+         var checkbox = d.createElement("input");
+              var deleteButton = d.createElement("button");
 
-    //event handling
-    addButton.addEventListener("click",function(){
-        if(textbox.value.trim()!="")
-        {
-            addItem(textbox.value);
-        }
-        else
-        {
-            alert("Please enter a todo item");
-        }
-    }); 
+     checkbox.addEventListener("change", function(){
+        if (!this.checked){
+            event.currentTarget.parentElement.remove();
 
-    function complete(event){
-         if (this.checked){
-            c.log("hey");
-    }
-    }
-    function removeItem(){
-        event.currenTarget.parentElement
-        todolist.removeChild(item);
+            addItemTodo(li.innerText);
+        }
+     
+    });
+     deleteButton.addEventListener("click",function (event){
+        event.currentTarget.parentElement.remove();
+        
+     });
+
+     deleteButton.innerText="-"
+    checkbox.type = "checkbox";
+    checkbox.checked=true;
+    li.appendChild(checkbox);
+
+     li.appendChild(value);
+     li.appendChild(deleteButton);
+        completedList.appendChild(li);
+        textbox.value="";
     }
 
 }
